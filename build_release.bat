678216b1c48b18f0b33292a5f49e096e4b96601a
edwardfang2018 <edwardfang2018@outlook.com>
2023-11-22 21:38:22 +0800

    Update Compilation Batch Processing

--- build_release.bat
+++ build_release.bat
@@ -7,7 +7,7 @@ if "%1"=="studio" (
     GOTO :studio
 )
 echo "building deps.."
-cmake ../ -G "Visual Studio 16 2019" -DDESTDIR="%CD%/OrcaSlicer_dep" -DCMAKE_BUILD_TYPE=Release
+cmake ../ -G "Visual Studio 17 2022" -DDESTDIR="%CD%/OrcaSlicer_dep" -DCMAKE_BUILD_TYPE=Release
 cmake --build . --config Release --target ALL_BUILD -- -m
 
 if "%1"=="deps" exit /b 0
@@ -18,6 +18,6 @@ cd %WP%
 mkdir build 
 cd build
 
-cmake .. -G "Visual Studio 16 2019" -DBBL_RELEASE_TO_PUBLIC=1 -DCMAKE_PREFIX_PATH="%DEPS%/usr/local" -DCMAKE_INSTALL_PREFIX="./OrcaSlicer" -DCMAKE_BUILD_TYPE=Release -DWIN10SDK_PATH="C:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0"
+cmake .. -G "Visual Studio 17 2022" -DBBL_RELEASE_TO_PUBLIC=1 -DCMAKE_PREFIX_PATH="%DEPS%/usr/local" -DCMAKE_INSTALL_PREFIX="./OrcaSlicer" -DCMAKE_BUILD_TYPE=Release -DWIN10SDK_PATH="C:/Program Files (x86)/Windows Kits/10/Include/10.0.22621.0"
 cmake --build . --config Release --target ALL_BUILD -- -m
 cmake --build . --target install --config Release
